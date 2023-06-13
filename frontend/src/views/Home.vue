f1d9990c48bf196aae9801f2b862fe01b5b7dd50
Jonathan T <84243060+jawtang@users.noreply.github.com>
2023-06-13 19:20:34 +0000

    updated title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,7 +7,7 @@
         <div id="top"></div>
         <div id="bottom"></div>
         <b-navbar-item id="fontnavbar" class="title is-2" style="color: rgb(224, 19, 19)">
-          Jawtang Miligram
+          JawTang Miligram
         </b-navbar-item>
       </template>
     </b-navbar>
